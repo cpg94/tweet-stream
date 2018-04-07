b677339f31a3db0e89a7caaca10598f80a3d161b
cpg94 <cpg_94@outlook.com>
2018-04-07 01:11:21 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tweet Streamer
+
+Simple Node script to watch topics on twitter.
+
+To start:
+
+` yarn start`
+
+Can watch as many different topics as you like, need to supply your own access tokens.
---
+++ index.js
@@ -0,0 +1,64 @@
+const Twit = require('twit');
+const inquirer = require('inquirer');
+
+async function main(){
+    console.log('What tweets do you want to watch?');
+    const question = [
+		{
+			message: "Name of tweet to watch.",
+			type: "input",
+			name: "answer"
+		},
+		{
+			message: "Another One?",
+			type: "confirm",
+			name: "confirm"
+    }];
+    
+    const answers = [];
+    askForTweet(question, answers)
+}
+
+async function askForTweet(questions, answers){
+    try {
+        const { answer, confirm } = await inquirer.prompt(questions);
+
+        if(!answer) {
+            console.log('Can not be blank!');
+            askForTweet(questions);
+        } else {
+            answers.push(answer);
+        }
+
+        if(confirm){
+            askForTweet(questions, answers);
+        } else {
+            startListeningForTweets(answers)
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function startListeningForTweets(track){
+    const t = new Twit({
+        consumer_key:         '',
+        consumer_secret:      '',
+        access_token:         '',
+        access_token_secret:  '',
+    });
+
+    const stream = t.stream('statuses/filter', { track });
+    console.log('Watching Tweets...')
+    stream.on('tweet', (tweet) => {
+        logTweet(tweet);
+    })
+}
+
+
+function logTweet(tweet){
+    console.log(`📣  - ${tweet.text}`)
+}
+
+main();
